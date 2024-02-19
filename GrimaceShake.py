7bae092718588032c9fd1629aeceabdfb5d5b8f7
Cowne <ashcowne@gmail.com>
2024-02-19 14:23:00 -0500

    Making Variable for the gas station
    test different variables for car gas and gas stations.

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -10,12 +10,28 @@ def gasLevelGauge():
     return currentGasLevel
 
 
-
 #Function that list Gas Stations, randomly choosing one and returning its value.
 def listOfGasStations():
     gasStations = ["Shell","Speedway","Marathon","Circle","K","Moble","Costco","Meijer","7Eleven",""]
     gasStationNearby = random.choice(gasStations)
     return gasStationNearby
+
+#Function will call the gasLevelGauge to determin out gas level and then
+# find the closest gas station by calling the listOfGasStations function if we are on low or quarter tank.
+def gasLevelAlert():
+    milesToGasStationsLow = random.uniform(1,25)
+    milesToGasStationsQuarterTank = random.uniform(25.1,50)
+    #gasLevelGauge = gasLevelGauge()
+    print(milesToGasStationsLow)
+    print(milesToGasStationsQuarterTank)
+
+gasLevelAlert()
+
+
+
+
+
+
 
 print(gasLevelGauge())
 print(listOfGasStations())
