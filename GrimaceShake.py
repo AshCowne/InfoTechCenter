56c52182356e8f0688b3ef155e470833bd76a3ff
Cowne <ashcowne@gmail.com>
2024-02-22 13:44:34 -0500

    Updated The Merge Branch

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -1,9 +1,38 @@
-print("*************************************************")
-print("Gasoline Branch\n\n")
-
-# Libraries Here
+#Import Libraries Here
+import time
+import sys
 import random
 from time import sleep
+
+
+# Welcome Branch starts here,
+timeToSleep = 1
+
+print("\n\nWelcome to InfoTech Center V1.0\n")
+time.sleep(timeToSleep)
+
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x +=  1
+    message = ("Infotech Center System Booting" + "." * ellipsis)
+    ellipsis = ellipsis + 1
+    sys.stdout.write("\r" + message) # \r prints a carriage return first
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\n\nOperating System Booted Up -Retina Scanned - Access Granted!")
+
+
+print("InfoTech Center Operating System Loading...")
+
+
+
+
+print("*************************************************\n")
+print("Gasoline Branch\n")
 
 def gasLevelGauge():
     gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
@@ -45,13 +74,3 @@ def gasLevelAlert():
         print("Your gas tank is full.")
 
 gasLevelAlert()
-
-
-
-
-
-
-
-#print(gasLevelGauge())
-#print(listOfGasStations())
-
