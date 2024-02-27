79c55f809c1920ff5d8902d75d77b9864c621b1d
Cowne <ashcowne@gmail.com>
2024-02-27 13:45:01 -0500

    Called sleep. still missing some code.

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -1,3 +1,6 @@
+print("Weather***********************************")
+
+
 retun
 
 
@@ -8,21 +11,40 @@ weatherAlert = weather()
 def vehicleResponseSystem():
     if weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 50mph.")
     elif weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 50mph.")
     elif weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 50mph.")
     elif weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 50mph.")
     elif weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 50mph.")
     elif weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 50mph.")
     elif weatherAlert == "foggy":
         print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
         print("VRS has been engaged only allowing you to drive 60mph")
     elif weatherAlert == "windy":
         print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 50mph.")
+    elif weatherAlert == "windy":
+        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 50mph.")
     else:
         print("\nNational Weather Service forecasts", weatherAlert, "weather conditions")
         print("VRS has been disengaged! Drive")
