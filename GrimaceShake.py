9136eb10c23a6f8c57017ae4d7e3e385edea8a28
Cowne <ashcowne@gmail.com>
2024-02-27 13:55:44 -0500

    Brought the code up to what everyone else has.

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -1,10 +1,18 @@
 print("Weather***********************************")
 
+print("Weather Branch\n")
 
-retun
+#Inport Libraries Here
+import random
+from time import sleep
 
+#Created a function that randomly chooses the weather from the list
+def weather():
+    weatherForcast = ["snowy", "blizzard", "rainy", "foggy", "windy", "icy", "sunny"]
+    weatherConditions = random.choice(weatherForcast)
+    return weatherConditions
 
-
+#Variable to call the weather() once VRS(Vehicle Response System) is determined
 
 weatherAlert = weather()
 
@@ -13,38 +21,26 @@ def vehicleResponseSystem():
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
         sleep(2)
         print("VRS has bean engaged only allowing you to drive 50mph.")
-    elif weatherAlert == "snowy":
+    elif weatherAlert == "blizzard":
+        print("\nNational Weather Service has updated our alarm by 45 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        sleep(2)
+        print("VRS has bean engaged only allowing you to drive 40mph.")
+    elif weatherAlert == "rainy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
         sleep(2)
-        print("VRS has bean engaged only allowing you to drive 50mph.")
-    elif weatherAlert == "snowy":
-        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
-        sleep(2)
-        print("VRS has bean engaged only allowing you to drive 50mph.")
-    elif weatherAlert == "snowy":
-        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
-        sleep(2)
-        print("VRS has bean engaged only allowing you to drive 50mph.")
-    elif weatherAlert == "snowy":
-        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
-        sleep(2)
-        print("VRS has bean engaged only allowing you to drive 50mph.")
-    elif weatherAlert == "snowy":
-        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
-        sleep(2)
-        print("VRS has bean engaged only allowing you to drive 50mph.")
+        print("VRS has bean engaged only allowing you to drive 60mph.")
     elif weatherAlert == "foggy":
-        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
         sleep(2)
-        print("VRS has been engaged only allowing you to drive 60mph")
+        print("VRS has bean engaged only allowing you to drive 30mph.")
     elif weatherAlert == "windy":
-        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        print("\nNational Weather Service has updated our alarm by 50 minutes because of the forcast of", weatherAlert,"weather conditions.")
         sleep(2)
         print("VRS has bean engaged only allowing you to drive 50mph.")
-    elif weatherAlert == "windy":
-        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of", weatherAlert,"weather conditions.")
+    elif weatherAlert == "icy":
+        print("\nNational Weather Service has updated our alarm by 50 minutes because of the forcast of", weatherAlert,"weather conditions.")
         sleep(2)
-        print("VRS has bean engaged only allowing you to drive 50mph.")
+        print("VRS has bean engaged only allowing you to drive 20mph.")
     else:
         print("\nNational Weather Service forecasts", weatherAlert, "weather conditions")
         print("VRS has been disengaged! Drive")
