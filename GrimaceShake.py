3e14bbeb44537149d44cca362962199b6af82d48
Cowne <ashcowne@gmail.com>
2024-02-27 13:36:10 -0500

    Started the Weather a day later so just puting in the code I know will be fixed in a later update.

---
+++ GrimaceShake.py
@@ -0,0 +1,29 @@
+retun
+
+
+
+
+weatherAlert = weather()
+
+def vehicleResponseSystem():
+    if weatherAlert == "snowy":
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+    elif weatherAlert == "snowy":
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+    elif weatherAlert == "snowy":
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+    elif weatherAlert == "snowy":
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+    elif weatherAlert == "snowy":
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+    elif weatherAlert == "snowy":
+        print("\nNational Weather Service has updated our alarm by 30 minutes because of the forcast of", weatherAlert,"weather conditions.")
+    elif weatherAlert == "foggy":
+        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of", weatherAlert,"weather conditions.")
+        print("VRS has been engaged only allowing you to drive 60mph")
+    elif weatherAlert == "windy":
+        print("\nNational Weather Service has updated our alarm by 10 minutes because of the forcast of", weatherAlert,"weather conditions.")
+    else:
+        print("\nNational Weather Service forecasts", weatherAlert, "weather conditions")
+        print("VRS has been disengaged! Drive")
+
